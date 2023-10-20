0fcc23305b385f6076c37d5ef505c7cc89fb4d88
adrienbuckland <bucklandadrien@gmail.com>
2023-10-20 13:04:32 +0100

    added extra stuff

--- week03.R
+++ week03.R
@@ -5,3 +5,8 @@ disastersfiltered <- disasters %>%
   select(Entity, Year, deaths_all_disasters, homeless_all_disasters, injured_all_disasters) %>%
   rename(total_deaths = deaths_all_disasters, total_homeless = homeless_all_disasters, total_injured = injured_all_disasters)
 view(disastersfiltered)
+disasterstopten <- disastersfiltered %>%
+  group_by(Entity) %>%
+  summarise(avg_deaths = mean(total_deaths, na.rm = TRUE) %>% round(2) #, avg_homeless = (mean(total_homeless, na.rm = TRUE) %>% round(2)), avg_injured = (mean(total_injured, na.rm = TRUE) %>% round(2))
+view(disasterstopten)
+summarise  
