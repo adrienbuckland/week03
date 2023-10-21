f88d83ce25d82c20a03f2bacdb64ada43d5d4ebf
adrienbuckland <bucklandadrien@gmail.com>
2023-10-21 20:55:18 +0100

    added extra stuff

--- week03.R
+++ week03.R
@@ -1,12 +1,32 @@
 #pacman::p_load(tidyverse)
+
 disasters <- read.csv("EMDAT.csv")
-view(disasters)
+
+#filtering to relevant variables
 disastersfiltered <- disasters %>%
   select(Entity, Year, deaths_all_disasters, homeless_all_disasters, injured_all_disasters) %>%
   rename(total_deaths = deaths_all_disasters, total_homeless = homeless_all_disasters, total_injured = injured_all_disasters)
-view(disastersfiltered)
-disasterstopten <- disastersfiltered %>%
+
+#finding per-year averages of relevant variables
+disastersavg <- disastersfiltered %>%
   group_by(Entity) %>%
-  summarise(avg_deaths = mean(total_deaths) %>% round(2), avg_homeless = mean(total_homeless) %>% round(2), avg_injured = mean(total_injured, na.rm = TRUE) %>% round(2))
-view(disasterstopten)
-summarise  
+  summarise(avg_deaths = mean(total_deaths) %>% round(2), avg_homeless = mean(total_homeless) %>% round(2), avg_injured = mean(total_injured, na.rm = TRUE) %>% round(2)) %>%
+  select(Entity, avg_deaths, avg_homeless, avg_injured)
+
+#sorting top ten highest average countries to relevant variables. row 1 excluded as it is always world average
+disasterstopdeaths <- disastersavg %>%
+  select(Entity, avg_deaths) %>%
+  arrange(desc(avg_deaths)) %>%
+  slice(2:11)
+disasterstophomeless <- disastersavg %>%
+  select(Entity, avg_homeless) %>%
+  arrange(desc(avg_homeless)) %>%
+  slice(2:11)
+disasterstopinjured <- disastersavg %>%
+  select(Entity, avg_injured) %>%
+  arrange(desc(avg_injured)) %>%
+  slice(2:11)
+
+disasterstopdeaths
+disasterstophomeless
+disasterstopinjured
